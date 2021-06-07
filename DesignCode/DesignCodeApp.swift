d4f592b653a113362d99d2ee9f714749435d9f17
Saramsvi <mscvitech@icloud.com>
2021-06-07 18:50:24 +0300

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by MSVI on 7.06.21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
